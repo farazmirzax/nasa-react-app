e04397b0c8d6115354688b5500fc9e205bf7cabb
Faraz Mirza <farazmirza1023@gmail.com>
2025-01-19 19:33:57 +0530

    I made some changes in the code so that it displays a video if it is sent through the API.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,25 @@
 export default function Main(props) {
-    const{data} = props;
+    const { data } = props;
+  
     return (
-        <div className="imgContainer">
-        <img src={data.hdurl} alt={data.title ||"bg-image"} 
-        className="bgImage"/>
-        </div>
-    )
-
+      <div className="imgContainer">
+        {data.media_type === "image" && (
+          <img
+            src={data.hdurl || data.url}
+            alt={data.title || "bg-image"}
+            className="bgImage"
+          />
+        )}
+        {data.media_type === "video" && (
+          <iframe
+            src={data.url}
+            title={data.title || "NASA Video"}
+            frameBorder="0"
+            allow="autoplay; encrypted-media"
+            className="bgVideo"
+          ></iframe>
+        )}
+      </div>
+    );
   }
+  
